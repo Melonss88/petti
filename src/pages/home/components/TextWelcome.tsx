1c39a97eb033b0e180bd186686fca561f2db0ab3
Melon <chiyudong@hotbit.pro>
2025-04-03 18:09:06 +0800

    only dist

--- src/pages/home/components/TextWelcome.tsx
+++
@@ -1,9 +0,0 @@
-const TextWelcome = () => {
-  return (
-    <h1 className="home-welcome text-gradient">
-      Welcome to Petti's home!
-    </h1>
-  );
-};
-
-export default TextWelcome;
--- src/utils/accountFormat.js
+++
@@ -1,4 +0,0 @@
-export const accountFormat = (account, start = 3, end = 4) => {
-  if (!account) return;
-  return account.slice(0, start) + "***" + account.slice(account.length - end);
-};
